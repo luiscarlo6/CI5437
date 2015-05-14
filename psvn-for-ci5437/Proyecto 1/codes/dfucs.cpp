608a517a0e48cb844055490d83eaf5f87097ee73
gabo1208 <gabo_h_d@hotmail.com>
2015-05-14 19:29:55 -0430

    ufcs y IDA* sin heuristicas

--- psvn-for-ci5437/Proyecto 1/codes/dfucs.cpp
+++ psvn-for-ci5437/Proyecto 1/codes/dfucs.cpp
@@ -1,33 +1,42 @@
 #include <vector>
 #include "priority_queue.hpp"
 #include <sys/time.h>
+#include <algorithm>    // std::min
 
-int visit(state_t* state, int64_t d, int64_t bound){
-    int64_t ruleid,t,p;
+int64_t visit(state_map_t* map, state_t* state, int64_t *bound){
+    int64_t ruleid, child_cost, state_cost, old_bound;
     ruleid_iterator_t iter;
     state_t child;
-    if (d > bound){
-        return d;
+    state_cost = *state_map_get(map, state);
+    int *best_child_dist;
+    if (state_cost > *bound){
+        *bound = state_cost;
+    }
+    if (is_goal(state)){
+        *bound = state_cost;
     }
 
-    if (is_goal(state)){
-        return -1;
-    }
-    t = 9223372036854775;
     init_fwd_iter( &iter, state );
     while( ( ruleid = next_ruleid( &iter ) ) >= 0 ) {
         apply_fwd_rule( ruleid, state, &child );
-    //printf("bound %"PRId64" , distance %"PRId64"  state ", bound,d );
-    //print_state(stdout,&child);
-    //printf("\n");
-        p = visit(&child, get_fwd_rule_cost(ruleid), bound);
-        if (p == -1){
-            *state = child;
-            return -1;
+        best_child_dist = state_map_get(map, &child);
+        child_cost = *bound + (int64_t)get_fwd_rule_cost(ruleid);
+        if(best_child_dist == NULL){
+            state_map_add( map, &child, child_cost );
+        }else if(*best_child_dist > child_cost){
+            state_map_add( map, &child, child_cost );
+        }else{
+            continue;
         }
-        if (t>p) t=p;
+
+        //printf("bound %"PRId64" , distance %"PRId64"  state ", child_cost, *best_child_dist);
+    //print_state(stdout,&child);
+        printf("\n");
+        if(child_cost > *bound)
+            *bound = (int64_t)child_cost;
+        old_bound = visit(map, state, bound);
     }
-    return t;
+    return *bound;
 }
 
 
@@ -35,8 +44,8 @@ int main( int argc, char **argv )
 {
     PriorityQueue<state_t> open;
     char raw_state[256];
-    int64_t totalNodes, numAtD;  // counters
-    state_t state, child, result;   // NOTE: "child" will be a predecessor of state, not a successor
+    int64_t totalNodes, numAtD, old_bound;  // counters
+    state_t root, child, result;   // NOTE: "child" will be a predecessor of state, not a successor
     int64_t d, ruleid, bound;
     ruleid_iterator_t iter;
     ssize_t nchars; 
@@ -47,16 +56,17 @@ int main( int argc, char **argv )
         if ( fgets(raw_state, sizeof raw_state, stdin) == NULL ) {
             return 0; 
         }
-        nchars = read_state( raw_state, &state );
+        nchars = read_state( raw_state, &root );
         bound = 0;
+        state_map_add( map, &root, bound );
         while (true){
-            d = visit(&state, 0, bound);
-            if (d==-1){
-                print_state( stdout, &state );
+            old_bound = visit(map, &root, &bound);
+            if (old_bound !=-1 || old_bound == bound){
+                print_state( stdout, &root );
                 printf("\n");
+                printf(" bound %"PRId64" \n", bound);
                 break;
             }
-            bound = d;
         }
 
     }
