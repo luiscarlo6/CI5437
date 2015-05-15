3789e8075c5a6d18de4110074b60254aaab76b56
Luiscarlo Rivera <luiscarlo@VITI>
2015-05-15 16:52:48 -0430

    something called A starmay seem to work, too slow :(

--- psvn-for-ci5437/Proyecto 1/15-puzzle/AStar.cpp
+++ psvn-for-ci5437/Proyecto 1/15-puzzle/AStar.cpp
@@ -8,47 +8,60 @@ int main( int argc, char **argv )
     char raw_state[256];
     int64_t totalNodes, numAtD;  // counters
     state_t state, child;   // NOTE: "child" will be a predecessor of state, not a successor
-    int64_t d, ruleid;
+    int64_t d, ruleid, current_distance,num_states;
+    int g, ng;
     ruleid_iterator_t iter;
     ssize_t nchars; 
+    timeval t1, t2;
+    double elapsedTime;
     state_map_t *map = new_state_map(); // contains the cost-to-goal for all states that have been generated
      // used for the states we have generated but not yet expanded (the OPEN list)
     FILE *file; // the final state_map is written to this file if it is provided (command line argument)
-    while (1) {
+    while (true) {
         if ( fgets(raw_state, sizeof raw_state, stdin) == NULL ) {
             return 0; 
         }
         nchars = read_state( raw_state, &state );
-        //print_state( stdout, &state );
-        //printf("->");
+        print_state( stdout, &state );
+        printf(" : - ");
+        gettimeofday(&t1, NULL);
+        num_states = 0;
+        state_map_add(map, &state, 0);
         open.Add(0, 0, state );
-        state_map_add( map, &state, 0 );
-        d = 0;
+        
         while( !open.Empty() ) {
+            ng = open.CurrentPriority();
             state = open.Top();
             open.Pop();
 
             if (is_goal(&state)){
-                print_state( stdout, &state );
-                printf("\n");
+                gettimeofday(&t2, NULL);
+                elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+                elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+                elapsedTime /= 1000;
+                const int *dist = state_map_get( map, &state );
+                printf("%i %"PRId64" %f %f\n",*dist, num_states, elapsedTime, num_states/elapsedTime);
                 open.Clear();
                 destroy_state_map(map);
                 map = new_state_map();
                 break;
             }
-            //printf("%i\n", d);
+
             init_fwd_iter( &iter, &state );
             while( ( ruleid = next_ruleid( &iter ) ) >= 0 ) {
                 apply_fwd_rule( ruleid, &state, &child );
-                const int *best_dist = state_map_get( map, &child );
-                if (best_dist == NULL){
-                    d++;
-                    open.Add(d,0, child );
-                    state_map_add( map, &child, d );
+                g = ng + get_fwd_rule_cost(ruleid);
+
+                const int * dist = state_map_get(map, &child);
+                if (dist == NULL){
+                    state_map_add(map, &child, g);
+                    open.Add(g, g, child);
                 }
-
+                else if (g < *dist) {
+                    state_map_add(map, &child, g);
+                    open.Modify(g, g, 0, child);
+                }
             }
-
         }
     }
         //printf("%i\n", d);
--- psvn-for-ci5437/Proyecto 1/codes/AStar-bak.cpp
+++ psvn-for-ci5437/Proyecto 1/codes/AStar-bak.cpp
@@ -8,51 +8,61 @@ int main( int argc, char **argv )
     char raw_state[256];
     int64_t totalNodes, numAtD;  // counters
     state_t state, child;   // NOTE: "child" will be a predecessor of state, not a successor
-    int64_t d, ruleid;
+    int64_t d, ruleid, current_distance,num_states;
+    int g;
     ruleid_iterator_t iter;
     ssize_t nchars; 
+    timeval t1, t2;
+    double elapsedTime;
     state_map_t *map = new_state_map(); // contains the cost-to-goal for all states that have been generated
      // used for the states we have generated but not yet expanded (the OPEN list)
     FILE *file; // the final state_map is written to this file if it is provided (command line argument)
-    while (1) {
+    while (true) {
         if ( fgets(raw_state, sizeof raw_state, stdin) == NULL ) {
             return 0; 
         }
         nchars = read_state( raw_state, &state );
-        //print_state( stdout, &state );
-        //printf("->");
-        open.Add(0, 0, state);
-        state_map_add( map, &state, 0 );
-        d = 0;
+        print_state( stdout, &state );
+        printf(" : - ");
+        gettimeofday(&t1, NULL);
+        num_states = 0;
+        open.Add(0, 0, state );
+        
         while( !open.Empty() ) {
-            /* get current distance from goal */
-            d = open.CurrentPriority();
+            g = open.CurrentPriority();
             state = open.Top();
             open.Pop();
 
-
             const int *best_dist = state_map_get( map, &state );
-            if (best_dist != NULL || d < *best_dist){
-                state_map_add(map, &state, d);
+            if ( best_dist == NULL || g < *best_dist ){
+                state_map_add( map, &state, g);
+                num_states++;
                 if (is_goal(&state)){
-                    print_state( stdout, &state );
-                    printf("\n");
+                    gettimeofday(&t2, NULL);
+                    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+                    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+                    elapsedTime /= 1000;
+                    const int *dist = state_map_get( map, &state );
+                    printf("%i %"PRId64" %f %f\n",*dist, num_states, elapsedTime, num_states/elapsedTime);
                     open.Clear();
                     destroy_state_map(map);
                     map = new_state_map();
                     break;
-                }   
+                }
 
-                 //printf("%i\n", d);
                 init_fwd_iter( &iter, &state );
                 while( ( ruleid = next_ruleid( &iter ) ) >= 0 ) {
                     apply_fwd_rule( ruleid, &state, &child );
-                    state_map_add(map, &child, d + get_fwd_rule_cost(ruleid) );
-                    open.Add(d + get_fwd_rule_cost(ruleid ),0,child);
+                 //   print_state(stdout,&child);
+                  //  printf("%i\n",g + get_fwd_rule_cost(ruleid));
+                    if (0 < 10000000){
+                        //state_map_add(map, &child, g + get_fwd_rule_cost(ruleid));
+                        open.Add(g + 0, g, child);
+                    }
                 }
-            }   
+            }  
         }
     }
-    //printf("%i\n", d);
+        //printf("%i\n", d);
     return 0;
 }
